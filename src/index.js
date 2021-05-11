c676d695c7a978f64a99ddfebb95b82989c10d97
Oj <oj@oojmed.com>
2021-05-11 09:49:38 +0100

    [Mods > VZ] Initial Add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { join } from 'path';
 
 import pc from './mods/pc.js';
 import bd from './mods/bd.js';
+import vz from './mods/vz.js';
+
 import readme from './doc/readme.js';
 import license from './doc/license.js';
 
@@ -14,6 +16,6 @@ const manifest = (await import(manifestPath)).default;
 const gitConfig = readFileSync(join(cwd, '.git', 'config'), 'utf8');
 const repo = gitConfig.match(/url = git@github.com:(.*).git/)[1];
 
-for (const func of [ pc, bd, readme, license ]) {
+for (const func of [ pc, bd, vz, readme, license ]) {
   func(cwd, manifest, repo);
 }
---
+++ src/mods/vz.js
@@ -0,0 +1,17 @@
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+export default (cwd, manifest, repo) => {
+  const vzManifest = {
+    name: manifest.name,
+    description: manifest.description,
+
+    version: manifest.version,
+    author: 'GooseNest',
+    license: 'MIT',
+
+    theme: `src/main.css`
+  };
+
+  writeFileSync(join(cwd, 'manifest.json'), JSON.stringify(vzManifest, null, 2));
+};
